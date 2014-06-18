21bf3e3e58c46fdb17e23079ce4c97473ec33afb
Harun Urhan <harunurhan17@gmail.com>
2014-06-18 15:50:22 +0300

    Initial Source Commit

---
+++ Assets/Scripts/GenerateWorld.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateWorld : MonoBehaviour {
+
+	public Transform player;
+	public Transform gold; // gold prefab
+	public Transform speedUp; // speedUp prefab
+	public Transform speedDown; // speedDown prefab
+	public Transform platform; // platform prefab
+	public Transform block;
+	private float randomRangeY = 2.0f; // how much up and down platforms will be.
+	private float platformSpawnedTo = 0.0f; // position of last spawned platform
+	private float nextCheck; // when to check.
+	private ArrayList platforms = new ArrayList(); // spawned platforms
+	private ArrayList collectables = new ArrayList(); // spawned collectables
+	private ArrayList blocks = new ArrayList(); // spawned blocks
+
+	// Update is called once per frame
+	void Update () {
+		float playerX = player.position.x;
+		if(playerX > nextCheck)
+		{
+			maintenance(playerX);
+		}
+	}
+
+	/* Checks if anything needed, for game world.
+	 * If needed, deletes game objects which are behind the player and screen.
+	 * If needed, spawns new platforms.
+	 */ 
+	private void maintenance(float playerX)
+	{
+		nextCheck = playerX + 30; // this 30, may be smaller or greater but not smaller than screen size.
+		for (int i = platforms.Count-1; i >= 0; i--) 
+		{
+			Transform p = (Transform)platforms[i];
+			if(p.position.x < (transform.position.x - 30)) // checks if game objects behind.
+			{
+				Destroy(p.gameObject);
+				platforms.RemoveAt(i);
+				collectables.RemoveAt(i);
+				blocks.RemoveAt(i);
+			}
+		}
+		spawnPlatforms(5); // why 5, because it is enough, player can't see more than 3 platforms at the same time.
+	}
+
+	/* Spawns platforms both randomly on x and on y.
+	 * Also spawns a collectable and a block for each new platform.
+	 * All positions calculated by using player's position.
+	 */
+	private void spawnPlatforms(int howMany)
+	{
+		float x = platformSpawnedTo;
+		for(int i = 0; i<howMany; i++)
+		{
+			float y = Random.Range(-randomRangeY,randomRangeY);
+			x += 8.0f;
+			Vector3 pos = new Vector3(x, y, 0);
+			Transform p = (Transform)Instantiate(platform, pos, Quaternion.identity);
+			platforms.Add(p);
+			Vector3 posCollectable = new Vector3(x+Random.Range(-3.0f,3.0f), y+Random.Range(0.9f,2.2f), 0);
+			spawnCollectable(posCollectable);
+			spawnBlock(pos);
+		}
+		platformSpawnedTo = x;
+	}
+
+	/* Spawns a block, on a platform.
+	 */
+	private void spawnBlock(Vector3 pos)
+	{
+		float rnd = Random.Range(-1.8f,1.8f);
+		pos.x+=rnd;
+		pos.y+=0.35f; 
+		Transform b = (Transform)Instantiate(block,pos,Quaternion.identity);
+		blocks.Add(b);
+
+	}
+	/* Spawns a collectable
+	 * It is 80% Gold, 10% SpeedUp, 10% SpeedDown. 
+	 */
+	private void spawnCollectable(Vector3 pos)
+	{
+		int random = Random.Range(1,10);
+		Transform collectable;
+		if(random<8)
+			collectable = (Transform)Instantiate(gold,pos, Quaternion.identity);
+		else if(random<9)
+			collectable = (Transform)Instantiate(speedUp,pos, Quaternion.identity);
+		else
+			collectable = (Transform)Instantiate(speedDown,pos, Quaternion.identity);
+		collectables.Add(collectable);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private float playerSpeed = 5.3f; //player's speed for x axis.
+	private bool isStartButtonClicked;
+	private int score = 0; // collected golds
+	private int health = 5;
+	public GUIText scoreLabel;
+	void Start () {
+		Time.timeScale=0.0f; // time stopped to wait for start click.
+		isStartButtonClicked = false;
+	}
+
+	void Update () {
+		if(!isInView()) gameOver();
+		applyInput();
+		updateScore();
+		transform.Translate(Vector3.right * playerSpeed * Time.deltaTime,Space.World); // go forward
+	}
+
+	/* Updates score label's text, 
+	 * using player's x axis position and number of gold collected so far to calculate current score.
+	 */
+	private void updateScore()
+	{
+		scoreLabel.text = ((int)(transform.position.x + score)).ToString();
+	}
+
+	/* Performs given input.
+	 * Different jump force for each input (A,S,D). Low,medium,high.
+	 */
+	private void applyInput()
+	{	
+		bool isGrounded = Physics.Raycast(transform.position, -Vector3.up, 0.4f); // check if player touches the ground.
+		if(Input.GetKeyDown(KeyCode.A)) jump(isGrounded,150);
+		else if(Input.GetKeyDown(KeyCode.S)) jump(isGrounded,260);
+		else if(Input.GetKeyDown(KeyCode.D)) jump(isGrounded,370);
+	}
+
+	/* Performs jump action
+	 * Jumps if player is grounded on Y axis.
+	 */
+	private void jump(bool isGrounded, float jumpPower)
+	{
+		if(isGrounded)
+		{
+			rigidbody.velocity = new Vector3(0,0,0);
+			rigidbody.AddForce (new Vector3(0,jumpPower,0), ForceMode.Force);
+			jumpPower = 0;
+		}
+	}
+
+	/* Checks if player is on the screen view.
+	 * Actually checks does player fall or not.
+	 */
+	private bool isInView()
+	{
+		Vector3 port = Camera.main.WorldToViewportPoint(transform.position);
+		if((port.x < 1) && (port.x > 0) && (port.y < 1) && (port.y > 0) && port.z > 0)
+			return true;
+		else
+			return false;
+	}
+
+	/* Game return the begining state, simply restarts.
+	 */
+	private void gameOver()
+	{
+		Application.LoadLevel (Application.loadedLevelName);
+		isStartButtonClicked = false;
+	}
+
+	/* Creates, Start Button and listens it.
+	 */
+	void OnGUI()
+	{
+		if(!isStartButtonClicked)
+		{
+			if(GUI.Button(new Rect(Screen.width/2-50,Screen.height/2+35,100,30), "Start"))
+			{
+				Time.timeScale = 1.0f; //start
+				isStartButtonClicked = true;
+			}
+		}
+	}
+
+	/* Occurs if player enters any trigger, more simply if player touches any trigger game object.
+	 * Trigger objects are collectables(SpeedUp, Gold, SpeedDown) and Block.
+	 * It also performs action for each trigger.
+	 */
+	void OnTriggerEnter(Collider other)
+	{
+		other.gameObject.SetActive(false); //disables entered game object, not visible anymore.
+		if(other.tag == "Gold")
+		{
+			score += 30;
+		}
+		else if(other.tag == "SpeedUp")
+		{
+			playerSpeed+=1f;
+		}
+		else if(other.tag == "SpeedDown")
+		{
+			playerSpeed-=1f;
+		}
+		else if(other.tag == "Block")
+		{
+			health--;
+			if(health <= 0) gameOver();
+		}
+	}
+}
